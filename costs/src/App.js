dfeed8e62310de2b3dba970e300e121d00a6a337
Thiago <thiagodessa41@gmail.com>
2022-06-27 17:08:58 -0300

    twenty-five

---
+++ costs/src/App.js
@@ -0,0 +1,33 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
+import NavBar from './components/layout/NavBar';
+import Footer from './components/layout/Footer';
+
+import Container from './components/layout/Container';
+import Projects from './components/pages/Projects';
+
+function App() {
+
+
+
+  return (
+    <Router>
+        <NavBar/>  
+    <Container customClass="min-height">
+      <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/company' element={<Company/>}></Route>
+          <Route path='/projects' element={<Projects/>}></Route>
+          <Route path='/contact' element={<Contact/>}></Route>
+          <Route path='/newproject' element={<NewProject/>}></Route>
+      </Routes>
+    </Container>
+        <Footer/>
+    </Router>
+  );
+}
+
+export default App;
