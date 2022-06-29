6b7220477b17fe45c51031ab0163f2aba97dd8a3
Thiago <thiagodessa41@gmail.com>
2022-06-29 18:07:18 -0300

    removerProjeto29

--- costs/src/components/project/ProjectCard.js
+++ costs/src/components/project/ProjectCard.js
@@ -4,6 +4,7 @@ import styles from './ProjectCard.module.css'
 import { BsPencil, BsFillTrashFill } from 'react-icons/bs'
 
 function ProjectCard({ id, name, budget, category, handleRemove }) {
+  
   const remove = (e) => {
     e.preventDefault()
     handleRemove(id)
@@ -19,11 +20,12 @@ function ProjectCard({ id, name, budget, category, handleRemove }) {
         <span className={`${styles[category.toLowerCase()]}`}></span> {category}
       </p>
       <div className={styles.project_card_actions}>
-        <Link to={'/'}>
-          <BsPencil/> Editar
+        <Link to={'/project/' + id}>
+          <BsPencil /> Editar
         </Link>
         <button onClick={remove}>
-          <BsFillTrashFill /> Excluir
+          <BsFillTrashFill />
+          Excluir
         </button>
       </div>
     </div>
